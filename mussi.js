3bb99f0c6056005de4d874b369c1929e5b5b3362
muskankhan-mfg <muskankhan95095@gmail.com>
2026-03-31 15:57:20 +0530

    maien ise file maien kuch bhi add kiya ho tume kya matab

--- mussi.js
+++ mussi.js
@@ -1,1 +1,5 @@
 console.log("mussi.... and junnu ")
+
+
+
+// dekho mussi git init karne se repostiory matab tere isme git folder create ho jata hai hna iske show nhi ho raha varna git karne se ek folder ban jata hai git name se vo tere maine hide hota hai thik hai ... fir status  aapun ko aapni xcurrnet repo ka status batati hai aap aapon ned ise file maien likh diya ye sab to aa raha hai modifted mussi.js thik hai aab ise add karna padega  ye add ho gyi dekho do type se add karte hai ek to git add <name> dusara bhut sari file ho to git add . ise se jitni bhi file tumne modfiy hai create ki hai vo sari ek sath add ho jaygi 
